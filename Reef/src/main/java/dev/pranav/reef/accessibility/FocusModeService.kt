268d49fa30785d13a500d621d126e41030c6b740
JosephMontfort <josephmontfort1607@gmail.com>
2026-05-09 20:41:53 +0530

    Add Notification Layout changes

--- Reef/src/main/java/dev/pranav/reef/accessibility/FocusModeService.kt
+++ Reef/src/main/java/dev/pranav/reef/accessibility/FocusModeService.kt
@@ -6,7 +6,9 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
-import android.content.Intent.FLAG_RECEIVER_FOREGROUND
+import android.widget.RemoteViews
+import android.content.Intent
+import android.widget.RemoteViews.FLAG_RECEIVER_FOREGROUND
 import android.content.pm.ServiceInfo
 import android.media.AudioAttributes
 import android.os.Build
