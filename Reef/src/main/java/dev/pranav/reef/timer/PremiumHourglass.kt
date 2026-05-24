eb06eb05c7b7ac429a4bd68c8ebd4b035d53dc17
JosephMontfort <josephmontfort1607@gmail.com>
2026-05-24 16:03:08 +0530

    Fix import Errors

--- Reef/src/main/java/dev/pranav/reef/timer/PremiumHourglass.kt
+++ Reef/src/main/java/dev/pranav/reef/timer/PremiumHourglass.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.graphicsLayer
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
